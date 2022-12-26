9fe6f65057a6222f6c26c0e6a8bff8e02fe7efdc
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-12-26 21:16:33 +0100

    Add: create variables to represnet various DOM elements

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+// Get the elements from the DOM
+const startBtn = document.querySelector('.start__btn');
+const resetBtn = document.querySelector('.reset__btn');
+const pauseBtn = document.querySelector('.pause__btn');
+const gameTimer = document.querySelector('.timer-content');
+const homeScore = document.querySelector('#score_home');
+const awayScore = document.querySelector('#score_guest');
+const homeScoringBtnCollection = document.querySelector('.home__scoring-btn').querySelectorAll('button');
+const awayScoringBtnCollection = document.querySelector('.away__scoring-btn').querySelectorAll('button');
+
 // Create a timer that runs from 0 to 12 minutes
 // at the end of the timer, score of the highest should be blinking with a color change to green
 // if the score is same, the color should be yellow for both team and the score should blink
