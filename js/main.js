8e41eece066aad3cb5fac495152bfa1b7d874e8f
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-12-26 21:16:33 +0100

    Add: create variables to represnet various DOM elements

--- js/main.js
+++ js/main.js
@@ -34,6 +34,8 @@ awayScoringBtnCollection.forEach((btn) => {
   });
 });
 
+
+
 // Create a timer that runs from 0 to 12 minutes
 // at the end of the timer, score of the highest should be blinking with a color change to green
 // if the score is same, the color should be yellow for both team and the score should blink
